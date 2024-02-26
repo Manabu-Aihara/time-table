18e73da933565850204dea8c321cb8c9a87bc956
Manabu-Aihara <manabu@yoboiryo.co.jp>
2024-02-26 11:55:26 +0900

    re-merge

--- src/components/EventsParent.tsx
+++ src/components/EventsParent.tsx
@@ -1,33 +1,18 @@
 import { createContext, Dispatch, ReactNode, useReducer } from 'react';
-<<<<<<< HEAD
-import { Event } from 'react-big-calendar';
-import moment from 'moment';
-
-type Events = Event[];
-=======
 // import { Event } from 'react-big-calendar';
 import { EventItem } from '../lib/EventItem';
 import moment from 'moment';
 
 type EventItems = EventItem[];
->>>>>>> origin/second
 
 // * State専用 Context *
 // 今後 Providerを使わない時にはContextの値がundefinedになる必要があるので, 
 // Contextの値がEventsにもundefinedにもできるように宣言してください。
-<<<<<<< HEAD
-export const EventsStateContext = createContext<Events | undefined>(undefined);
-
-type Action = 
-  | { type: 'CREATE'; title: string }
-  | { type: 'REMOVE'; title: string };
-=======
 export const EventsStateContext = createContext<EventItems | undefined>(undefined);
 
 type Action = 
   | { type: 'CREATE'; payload: {title: ReactNode} }
   | { type: 'UPDATE'; payload: EventItem };
->>>>>>> origin/second
 
 type EventsDispatch = Dispatch<Action>;
 
@@ -37,20 +22,6 @@ export const EventsDispatchContext = createContext<EventsDispatch | undefined>(
 );
 
 // * Reducer *
-<<<<<<< HEAD
-export function eventsReducer (state: Events, action: Action): Events {
-
-  switch (action.type) {
-    case 'CREATE':
-      console.log("呼ばれました");
-      return state.concat({
-          title: action.title,
-          start: new Date(),
-          end: new Date(new Date().setHours(new Date().getHours() + 1)),    
-        });
-    // case 'REMOVE':
-    //   return state.filter(state => state.title !== action.title);
-=======
 export function eventsReducer (eventState: EventItems, action: Action): EventItems {
   const { type, payload } = action;
 
@@ -63,7 +34,6 @@ export function eventsReducer (eventState: EventItems, action: Action): EventIte
         });
     case 'UPDATE':
       return eventState.map(evt => evt.title === action.payload.title ? action.payload : evt)
->>>>>>> origin/second
     default:
       throw new Error('Invalid action');
   }
